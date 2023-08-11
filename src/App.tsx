e3102eacc168271a32963ce32fcdc4ce46afbe9c
gureumwoon <vfrvfr3221@gmail.com>
2023-08-11 18:20:43 +0900

    [Feat] SignIn Router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
       </Routes>
     </div>
   );
