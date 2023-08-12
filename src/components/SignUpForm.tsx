9547e4c575c683ec684bb3ef0c005cb144e809b6
gureumwoon <vfrvfr3221@gmail.com>
2023-08-12 16:30:52 +0900

    [Feat] 회원가입 기능 완성

--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,13 +1,78 @@
+import { useNavigate } from "react-router-dom"
 import { styled } from "styled-components";
 import Input from "../elements/Input";
 import Button from "../elements/Button";
+import { useUserForm } from "../hooks/useUserForm";
+import { apis } from "../apis/api";
 
 function SignUpForm() {
+    const navigate = useNavigate();
+    const { userData, handleEmailChange, isEmail, handlePasswordChange, isPassword } = useUserForm({
+        email: '',
+        password: '',
+    });
+
+    const handleCheckBtn = () => {
+        let offButton = false
+        if (isEmail === false && isPassword === false) {
+            offButton = true;
+        } else if (userData.email === "" || userData.password === "") {
+            offButton = true;
+        } else if (isEmail === true && isPassword === true) {
+            offButton = false;
+        }
+        return offButton;
+    }
+
+
+    const handleSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        console.log(isEmail, isPassword)
+        if (isEmail === false || isPassword === false) {
+            window.alert("이메일/비밀번호 형식에 맞게 입력해 주세요.")
+        }
+        apis.signUp(userData)
+            .then(() => {
+                window.alert("회원가입 성공");
+                navigate("/signin")
+            }).catch((error) => {
+                if (error.response.status === 400) {
+                    window.alert("동일한 이메일이 존재합니다.")
+                }
+                console.log(error);
+            })
+    }
+
+
     return (
-        <SignUpFormField>
-            <Input size="medium" margin="0 0 15px" placeholder="이메일을 입력해 주세요." />
-            <Input size="medium" type="password" placeholder="비밀번호를 입력해 주세요." />
-            <Button size="medium" margin="30px 0 15px">SignUp</Button>
+        <SignUpFormField onSubmit={handleSignUp}>
+            <Input
+                dataTestId="email-input"
+                name="email"
+                value={userData.email}
+                size="medium"
+                type="text"
+                margin="0 0 15px"
+                placeholder="이메일을 입력해 주세요."
+                _onChange={handleEmailChange}
+            />
+            <Input
+                dataTestId="password-input"
+                name="password"
+                value={userData.password}
+                size="medium"
+                type="password"
+                placeholder="비밀번호를 8자 이상 입력해 주세요."
+                _onChange={handlePasswordChange}
+            />
+            <Button
+                dataTestId="signup-button"
+                size="medium"
+                margin="30px 0 15px"
+                _disabled={handleCheckBtn()}
+            >
+                SignUp
+            </Button>
         </SignUpFormField>
     )
 }
