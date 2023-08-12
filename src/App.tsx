72e5c8f0c0b2eba3c7b80d876de3fcc785abf5fe
gureumwoon <vfrvfr3221@gmail.com>
2023-08-12 23:51:56 +0900

    [Feat] /todo Router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import Todo from './pages/Todo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
+        <Route path='/todo' element={<Todo />} />
       </Routes>
     </div>
   );
