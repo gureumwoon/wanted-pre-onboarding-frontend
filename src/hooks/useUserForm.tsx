f78cee2ea4a0def035a88ea9eb9f2494b8ea7b8e
gureumwoon <vfrvfr3221@gmail.com>
2023-08-12 16:32:35 +0900

    [Feat] useUserForm 커스텀 훅에 email, password 유효성 검사 기능 추가

---
+++ src/hooks/useUserForm.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { UserRequest } from '../types/elements/User';
+
+export const useUserForm = (initialValues: UserRequest) => {
+    const [userData, setUserData] = useState(initialValues);
+    const [isEmail, setIsEmail] = useState(false);
+    const [isPassword, setIsPassword] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const isEmailCheck = (email: string) => {
+        const regEmail = /@/;
+        if (regEmail.test(email)) {
+            setIsEmail(true);
+        } else {
+            setIsEmail(false);
+        }
+    }
+
+    const isPasswordCheck = (password: string) => {
+        const regPassword = /^.{8,}$/;
+        if (regPassword.test(password)) {
+            setIsPassword(true);
+        } else {
+            setIsPassword(false);
+        }
+    }
+
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        isEmailCheck(value);
+        handleChange(e);
+    };
+
+    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        isPasswordCheck(value);
+        handleChange(e);
+    };
+
+    return {
+        userData,
+        handleChange,
+        handleEmailChange,
+        isEmail,
+        handlePasswordChange,
+        isPassword
+    };
+};
+
+
