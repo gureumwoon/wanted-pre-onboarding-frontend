3ed3b288ed0e8659dd305a86c9b5a287e1c63a85
gureumwoon <vfrvfr3221@gmail.com>
2023-08-14 02:05:37 +0900

    [Feat] ButtonProps interface에 타입 추가

--- src/types/elements/ButtonType.tsx
+++ src/types/elements/ButtonType.tsx
@@ -11,5 +11,5 @@ export interface ButtonProps extends ButtonStyleProps {
     dataTestId: string;
     children: string;
     _onClick?: React.MouseEventHandler<HTMLElement>;
-    _disabled: boolean;
+    _disabled?: boolean;
 }
