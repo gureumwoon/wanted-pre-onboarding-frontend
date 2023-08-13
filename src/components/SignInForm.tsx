0524c03ad9f295d2741e65f299e3899b27a9a1fd
gureumwoon <vfrvfr3221@gmail.com>
2023-08-14 02:09:19 +0900

    [Refactor] localStorage 코드 변경

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,13 +1,68 @@
+import { useNavigate } from "react-router-dom"
 import { styled } from "styled-components";
 import Input from "../elements/Input";
 import Button from "../elements/Button";
+import { useUserForm } from "../hooks/useUserForm";
+import { apis } from "../apis/api";
 
 function SignInForm() {
+    const navigate = useNavigate()
+    const { userData, handleEmailChange, handlePasswordChange } = useUserForm({
+        email: '',
+        password: ''
+    })
+
+    const handleCheckBtn = () => {
+        let offButton = false
+        if (userData.email === "" || userData.password === "") {
+            offButton = true;
+        } else {
+            offButton = false;
+        }
+        return offButton;
+    }
+
+    const handleSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        apis.signIn(userData)
+            .then((res) => {
+                localStorage.setItem("access_token", res.data.access_token);
+                window.alert("환영합니다.")
+                navigate("/")
+            }).catch((error) => {
+                window.alert("이메일/비밀번호를 확인해 주세요.")
+                console.log(error);
+            })
+
+    }
+
     return (
-        <SignInFormField>
-            <Input size="medium" margin="0 0 15px" placeholder="이메일을 입력해 주세요." />
-            <Input size="medium" type="password" placeholder="비밀번호를 입력해 주세요." />
-            <Button size="medium" margin="30px 0 15px">SignIn</Button>
+        <SignInFormField onSubmit={handleSignIn}>
+            <Input
+                dataTestId="email-input"
+                name="email"
+                size="medium"
+                margin="0 0 15px"
+                placeholder="이메일을 입력해 주세요."
+                _onChange={handleEmailChange}
+            />
+            <Input
+                dataTestId="password-input"
+                name="password"
+                size="medium"
+                type="password"
+                placeholder="비밀번호를 입력해 주세요."
+                _onChange={handlePasswordChange}
+            />
+            <Button
+                dataTestId="signin-button"
+                size="medium"
+                margin="30px 0 15px"
+                _disabled={handleCheckBtn()}
+            >
+                SignIn
+            </Button>
         </SignInFormField>
     )
 }
