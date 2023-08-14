353cc2f2224e3dcb6ad2a8032815cec8ed34172a
gureumwoon <vfrvfr3221@gmail.com>
2023-08-14 20:08:07 +0900

    [Feat] token 여부에 따라 redirect 설정 해주기

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Todo from './pages/Todo';
+import Home from './pages/Home';
 
 function App() {
+  const token = localStorage.getItem("access_token");
   return (
     <div className="App">
       <Routes>
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/todo' element={<Todo />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/signup' element={token ? <Navigate to="/todo" /> : <SignUp />} />
+        <Route path='/signin' element={token ? <Navigate to="/todo" /> : <SignIn />} />
+        <Route path='/todo' element={token ? <Todo /> : <Navigate to="/signin" />} />
       </Routes>
     </div>
   );
