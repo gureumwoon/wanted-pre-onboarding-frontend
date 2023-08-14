27ee179c45f32a677e7e585da81658edbd0355cb
gureumwoon <vfrvfr3221@gmail.com>
2023-08-14 20:07:36 +0900

    [Design] / 경로 UI 완성

---
+++ src/pages/Home.tsx
@@ -0,0 +1,75 @@
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom"
+import Button from "../elements/Button";
+
+function Home() {
+    const navigate = useNavigate()
+    return (
+        <Main>
+            <SignUp>
+                <p>회원이 아니신가요?</p>
+                <Button size="large" _onClick={() => navigate("/signup")}>회원가입</Button>
+            </SignUp>
+            <User>
+                <p>회원이신가요?</p>
+                <div>
+                    <Button size="large" _onClick={() => navigate("/signin")}>로그인</Button>
+                    <Button size="large" _onClick={() => navigate("/todo")}>TodoList</Button>
+                </div>
+            </User>
+        </Main>
+    )
+}
+
+const Main = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: cornflowerblue;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  
+`
+const SignUp = styled.div`
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    margin-bottom: 10px;
+    p {
+        font-size: 2rem;
+        margin-right: 80px;
+    }
+    button {
+        background-color: #F7EA87;
+        color: #000
+    }
+`
+
+const User = styled.div`
+    width: 70%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    margin-top: 10px;
+    p{
+        font-size: 2rem;
+        margin-right: 80px;
+    }
+    div {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    button {
+        background-color: #F7EA87;
+        color: #000
+    }
+    button:first-child {
+        margin-right: 20px;
+    }
+    }
+`
+
+export default Home
