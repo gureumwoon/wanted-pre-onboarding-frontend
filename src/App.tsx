a79ccde6ae817f557a3ce295f91b3346a88ad561
gureumwoon <vfrvfr3221@gmail.com>
2023-08-15 02:38:58 +0900

    [Feat] token 유무에 따른 접근경로 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import SignUp from './pages/SignUp';
@@ -6,14 +8,22 @@ import Todo from './pages/Todo';
 import Home from './pages/Home';
 
 function App() {
+  const navigate = useNavigate();
   const token = localStorage.getItem("access_token");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/todo");
+    }
+  }, []);
+
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/signup' element={token ? <Navigate to="/todo" /> : <SignUp />} />
-        <Route path='/signin' element={token ? <Navigate to="/todo" /> : <SignIn />} />
-        <Route path='/todo' element={token ? <Todo /> : <Navigate to="/signin" />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path="/todo" element={!token ? <Navigate to="/signin" /> : <Todo />} />
       </Routes>
     </div>
   );
